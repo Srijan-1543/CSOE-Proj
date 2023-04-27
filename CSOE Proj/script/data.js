5ff3f2fc5f9ef2cfa57e65b3f5e6f6500147b595
Srijan-1543 <98376263+Srijan-1543@users.noreply.github.com>
2023-04-28 00:23:03 +0530

    Add files via upload

---
+++ CSOE Proj/script/data.js
@@ -0,0 +1,59 @@
+let shopItemsData = [
+  {
+    id: "food-1",
+    name: "Egg Puff",
+    price: 15,
+    desc: "",
+    img: "../image/eggpuff.jpg",
+  },
+  {
+    id: "food-2",
+    name: "Chicken Roll",
+    price: 20,
+    desc: "",
+    img: "../image/chickenroll.jpg",
+  },
+  {
+    id: "food-3",
+    name: "Cool Drink",
+    price: 20,
+    desc: "",
+    img: "../image/cooldrink.jpg",
+  },
+  {
+    id: "food-4",
+    name: "Chocolate",
+    price: 25,
+    desc: "",
+    img: "../image/chocolate.jpg",
+  },
+  {
+    id: "food-5",
+    name: "Sandwich",
+    price: 20,
+    desc: "",
+    img: "../image/sandwich.jpg",
+  },
+  
+  {
+    id: "food-6",
+    name: "Biscuit",
+    price: 10,
+    desc: "",
+    img: "../image/biscuit.jpg",
+  },
+  {
+    id: "food-7",
+    name: "Omelette",
+    price: 15,
+    desc: "",
+    img: "../image/omelette.jpg",
+  },
+  {
+    id: "food-8",
+    name: "Coffee",
+    price: 12,
+    desc: "",
+    img: "../image/coffee.jpg",
+  },
+];
